b5d777e24d900661857614bfae7096190a26ef88
Jack Mordaunt <jackmordaunt.dev@gmail.com>
2024-05-03 16:18:32 +0800

    cmd/nativeaudio: allow conversion
    Convert the audio file instead of playing it, if out is specified.

--- cmd/nativeaudio/main.go
+++ cmd/nativeaudio/main.go
@@ -8,10 +8,14 @@ import (
 	"git.sr.ht/~jackmordaunt/nativeaudio"
 )
 
-var in string
+var (
+	in  string
+	out string
+)
 
 func main() {
-	flag.StringVar(&in, "in", "", "input audio file to play")
+	flag.StringVar(&in, "in", "", "input audio file")
+	flag.StringVar(&out, "out", "", "output pcm file, will play audio if out not specified")
 	flag.Parse()
 	if err := run(); err != nil {
 		fmt.Printf("error: %v\n", err)
@@ -20,5 +24,18 @@ func main() {
 }
 
 func run() error {
-	return nativeaudio.Play(in)
+	if out != "" {
+		un, _, err := nativeaudio.Load(in)
+		if err != nil {
+			return fmt.Errorf("loading audio file: %w", err)
+		}
+		if err := os.WriteFile(out, un, 0o644); err != nil {
+			return fmt.Errorf("writing output pcm file: %w", err)
+		}
+		return nil
+	}
+	if err := nativeaudio.Play(in); err != nil {
+		return fmt.Errorf("playing audio file: %w", err)
+	}
+	return nil
 }
